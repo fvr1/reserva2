bbed079cfd93e486fe609c65ec6d38243374b3c8
Stephanie Chau <schau@uc.cl>
2019-05-06 11:06:44 -0400

    Deleted CRUD test files

--- spec/controllers/resource_controller/edit_spec.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literals: true
-
-require 'rails_helper'
-
-RSpec.describe ResourceController, type: :controller do
-  
-end
--- spec/controllers/resources_controller_spec.rb
+++ spec/controllers/resources_controller_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe ResourcesController, type: :controller do
+  describe 'GET resources#index' do
+  end
+  describe 'POST resources#create' do
+  end
+  describe 'PATCH resources#update' do
+  end
+  describe 'DELETE resources#destroy' do
+  end
 
 end
