ddd3eeccd9749ed5ed96a1c7178ac2309be035fd
Stephanie Chau <schau@uc.cl>
2019-05-11 17:51:16 -0400

    Added more test strucutres

--- spec/controllers/resources_controller_spec.rb
+++ spec/controllers/resources_controller_spec.rb
@@ -1,13 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe ResourcesController, type: :controller do
-  describe 'GET resources#index' do
+  describe "Methods" do
+    describe "GET resources#index" do
+      context "when there is no user" do
+        pending "no user"
+      end
+      context "when there user" do
+        pending "user"
+      end
+    end
+    describe "POST resources#create" do
+      pending "new resource"
+    end
+    describe "PATCH resources#update" do
+      pending "update resource"
+    end
+    describe "DESTROY resources#destroy" do
+      pending "delete resource"
+    end
   end
-  describe 'POST resources#create' do
-  end
-  describe 'PATCH resources#update' do
-  end
-  describe 'DELETE resources#destroy' do
+  describe "Private params" do
+    describe "wrong params" do
+    end
+
+    describe "correct params" do
+    end
   end
 
 end
