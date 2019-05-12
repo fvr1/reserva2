8057ad851b6d27d5b732588a6d301ef42478b9d0
Stephanie Chau <schau@uc.cl>
2019-05-11 21:13:06 -0400

    Added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+p '==== Create one company ===='
+
+companies = Company.create({ name: 'Reservaditos', phone: '+56987621222',
+  address: 'Manzano 343, Recoleta, RM, Chile', mail: 'reserva2@uc.cl',
+  description: 'Una gran empresa de reservas'
+})
+
+
+p '==== Create multiple resources ===='
+
+Resource.create({ name: 'Florencia Vayadares', category: 'Organizer',
+  details: 'The best organizer in town', company: companies
+})
+
+Resource.create({ name: 'Felipe Garriding', category: 'Organizer',
+  details: 'The 2nd best organizer in town', company: companies
+})
+
+Resource.create({ name: 'Stephanie Chaucha', category: 'Organizer',
+  details: 'The 3rd best organizer in town', company: companies
+})
