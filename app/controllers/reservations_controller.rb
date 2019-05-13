abe40d567bbe6cf163e5c0c4e731e8a3cc9b815e
Stephanie Chau <schau@uc.cl>
2019-05-13 11:08:07 -0400

    Added reservation service and controller

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,26 @@
 class ReservationsController < ApplicationController
+  def index
+    @reservations = ReservationService.index
+    render json: @reservations, status: 200
+  end
+  def show
+    @reservation = ReservationService.show(params)
+    @reservation ? render_json(@reservation, 200) : render_json({}, 404)
+  end
+  def create
+    @reservation = ReservationService.create(params)
+    @reservation ? render_json(@reservation, 200) : render_json({}, :unprocessable_entity)
+  end
+  def update
+    @reservation = ReservationService.update(params)
+    @reservation ? render_json(@reservation, 200) : render_json({}, :unprocessable_entity)
+  end
+  def destroy
+    @reservation = ReservationService.destroy(params)
+    @reservation == {} ? render_json({}, 200) : render_json({}, 500)
+  end
+  private
+  def render_json(reservation, status)
+    render json: reservation, status: status
+  end
 end
