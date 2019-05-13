de30305af5324d876bb93f40a20daa390852bce3
Florencia Valladares <fvalladares1@uc.cl>
2019-05-13 19:54:52 -0400

    add logic of disp and not disp

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -10,6 +10,8 @@ class CompaniesController < ApplicationController
     # TODO: change resource
     @resource = @company.resources.first
     @blocks = @company.blocks
+    reservations = @resource.states.where('date IS NOT NULL').where('date > ?', 0.days.from_now).where('date < ?', 90.days.from_now)
+    other_ocupation = @resource.states.where('date IS NULL')
 
     @all_days = []
     (0..90).each do |n|
@@ -18,14 +20,30 @@ class CompaniesController < ApplicationController
 
     @data_blocks = []
     @blocks.each do |block|
-      start_hour = block.start_time.strftime('%H')
-      start_minute = block.start_time.strftime('%M')
       @all_days.each do |day|
         if (day.strftime('%A').downcase == block.day)
+          available = true
+          other_ocupation.each do |ocupation| 
+            if (ocupation.block.id == block.id)
+              available = false
+            end
+          end
+
+          reservations.each do |reservation| 
+            if (reservation.block.id == block.id)
+              same_day = reservation.date.day == day.day
+              same_month = reservation.date.month == day.month
+              same_year = reservation.date.year == day.year
+              if (same_day && same_month && same_year)
+                available = false
+              end
+            end
+          end
 
           @data_blocks.push(HelperBlock.new(DateTime.new(day.year, day.month, day.day, block.start_time.hour, block.start_time.min, 0),
           DateTime.new(day.year, day.month, day.day, block.end_time.hour, block.end_time.min, 0),
-          false))
+          available))
+
         end
       end
     end
