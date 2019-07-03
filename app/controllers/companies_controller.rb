fc75b7d05503bcdf32a6c70d1e9c61bc50f084ec
Felipe Garrido <figarrido@uc.cl>
2019-07-03 17:17:41 -0400

    Style forms

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -2,11 +2,14 @@ class CompaniesController < ApplicationController
   def index
     @company = CompanyService.show(params)
     @data_blocks = generate_blocks(@company)
+    @resource = @company.resources.first
+    @state = State.new
   end
 
   def show
     @company = CompanyService.show(params)
     @data_blocks = generate_blocks(@company)
+    @resource = @company.resources.first
     @state = State.new
   end
 
@@ -15,7 +18,6 @@ class CompaniesController < ApplicationController
     @block = Block.new
     blocks = @company.blocks
     @all_days = (-20..20).map { |n| n.days.from_now }
-
 
     @data_blocks = []
     blocks.each do |block|
@@ -75,13 +77,13 @@ def generate_blocks(company)
         same_month = reservation.date.month == day.month
         same_year = reservation.date.year == day.year
         if same_day && same_month && same_year
-          hb.available = false 
-          hb.reservation = reservation.reservation 
+          hb.available = false
+          hb.reservation = reservation.reservation
         end
       end
 
       @data_blocks.push(hb)
     end
   end
-  return @data_blocks
+  @data_blocks
 end
