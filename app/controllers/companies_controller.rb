9f30d4c3dfc1ff8b1abd05c9e7e450c9ac64b2e8
Florencia Valladares <fvalladares1@uc.cl>
2019-07-03 14:23:16 -0400

    merge

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,48 +1,7 @@
 class CompaniesController < ApplicationController
   def index
     @company = CompanyService.show(params)
-    # TODO: change resource
-    @resource = @company.resources.first
-    blocks = @company.blocks
-    reservations = @resource.states.where('date IS NOT NULL').where('date >= ?', Date.today).where('date < ?', 90.days.from_now)
-    other_ocupation = @resource.states.where('date IS NULL')
-    @all_days = (0..90).map { |n| n.days.from_now }
-
-    @data_blocks = []
-    blocks.each do |block|
-      @all_days.each do |day|
-        if (day.strftime('%A').downcase == block.day)
-          available = true
-          hb = HelperBlock.new(
-            DateTime.new(day.year, day.month, day.day, block.start_time.hour, block.start_time.min, 0),
-            DateTime.new(day.year, day.month, day.day, block.end_time.hour, block.end_time.min, 0),
-            available,
-            block
-          )
-
-          other_ocupation.each do |ocupation|
-            if (ocupation.block.id == block.id)
-              hb.available = false
-              hb.ocupation = ocupation
-            end
-          end
-
-          reservations.each do |reservation|
-            if (reservation.block.id == block.id)
-              same_day = reservation.date.day == day.day
-              same_month = reservation.date.month == day.month
-              same_year = reservation.date.year == day.year
-              if (same_day && same_month && same_year)
-                hb.available = false
-                hb.reservation = reservation.reservation
-              end
-            end
-          end
-
-          @data_blocks.push(hb)
-        end
-      end
-    end
+    @data_blocks = generate_blocks(@company)
   end
 
   def show
@@ -95,4 +54,54 @@ class CompaniesController < ApplicationController
   def new
     @block = Block.new
   end
+end
+
+
+def generate_blocks(company)
+  @resource = company.resources.first
+
+  if @resource.nil?
+    @data_blocks = []
+    @resource = company.resources.build
+  end
+
+  blocks = company.blocks
+  reservations = @resource
+                 .states
+                 .where('date IS NOT NULL')
+                 .where('date >= ?', Date.today)
+                 .where('date < ?', 90.days.from_now)
+  other_ocupation = @resource.states.where('date IS NULL')
+  @all_days = (0..90).map { |n| n.days.from_now }
+
+  @data_blocks = []
+  blocks.each do |block|
+    @all_days.each do |day|
+      next unless day.strftime('%A').downcase == block.day
+
+      available = true
+      hb = HelperBlock.create_block(block, day, available)
+      other_ocupation.each do |ocupation|
+        if ocupation.block.id == block.id
+          hb.available = false
+          hb.ocupation = ocupation
+        end
+      end
+
+      reservations.each do |reservation|
+        next unless reservation.block.id == block.id
+
+        same_day = reservation.date.day == day.day
+        same_month = reservation.date.month == day.month
+        same_year = reservation.date.year == day.year
+        if same_day && same_month && same_year
+          hb.available = false 
+          hb.reservation = reservation.reservation 
+        end
+      end
+
+      @data_blocks.push(hb)
+    end
+  end
+  return @data_blocks
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  root to: redirect('/company/1')
   devise_for :users
 
   devise_scope :user do
     root to: 'devise/sessions#new'
+  end
 
-  get '/company/:id', to: 'companies#index'
-  get '/company/:id/reservations', to: 'companies#show'
-  get '/company', to: 'companies#new', as: :create_company_path
+  get '/companies/:id', to: 'companies#index'
+  get '/companies/:id/reservations', to: 'companies#show',  as: :company
+  get '/companies', to: 'companies#new', as: :create_company_path
   get '/reservation/:id/done', to: 'reservations#done', as: :done_reservation
 
   post '/states', to: 'states#create', as: :states
